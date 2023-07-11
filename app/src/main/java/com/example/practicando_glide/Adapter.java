d823a11e8af2ef96d5852c901780166741d29496
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-11 00:23:53 -0400

    Se implementa método onCreteViewHolder();

--- app/src/main/java/com/example/practicando_glide/Adapter.java
+++ app/src/main/java/com/example/practicando_glide/Adapter.java
@@ -1,10 +1,13 @@
 package com.example.practicando_glide;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.practicando_glide.databinding.ItemBinding;
 
 import java.util.List;
 
@@ -13,7 +16,8 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     @NonNull
     @Override
     public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        ItemBinding binding = ItemBinding.inflate(LayoutInflater.from(parent.getContext()));
+        return new ViewHolder(binding);
     }
 
     @Override
@@ -27,8 +31,9 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
+
+        public ViewHolder(ItemBinding binding) {
+            super(binding.getRoot());
         }
     }
 }
