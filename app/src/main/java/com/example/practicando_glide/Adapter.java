35e71cfa0cdd0fbc11faf33a42a3791625d905a8
Flopy <f.hidalgoalarcon@gmail.com>
2023-07-13 13:52:06 -0400

    Se modifica para pasar de FirstFragment a SecondFragment.

--- app/src/main/java/com/example/practicando_glide/Adapter.java
+++ app/src/main/java/com/example/practicando_glide/Adapter.java
@@ -2,9 +2,14 @@ package com.example.practicando_glide;
 
 import android.media.Image;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
@@ -13,7 +18,7 @@ import com.example.practicando_glide.databinding.ItemBinding;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
     private List<Imagen> image = new ArrayList<>();
 
     @NonNull
@@ -28,7 +33,6 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         Imagen item = image.get(position);
         holder.bind(item);
 
-
     }
 
     @Override
@@ -39,17 +43,26 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         this.image = item;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     private ItemBinding binding;
 
         public ViewHolder(ItemBinding binding) {
             super(binding.getRoot());
             this.binding = binding;
+            itemView.setOnClickListener(this);
+
         }
 
         public void bind(Imagen image) {
             binding.textViewImage.setText(image.text);
             Glide.with(binding.getRoot()).load(image.url).into(binding.imageViewCard);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Navigation.findNavController(binding.getRoot()).navigate(R.id.action_firstFragment_to_secondFragment);
         }
     }
 }
